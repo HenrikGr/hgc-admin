f99c5d805f9127323472202e9a036a42a8cf903c
HenrikGr <henrik.gronvall@bredband.net>
2018-03-04 21:37:59 +0100

    Commit and push updates

--- src/modules/api/auth.js
+++ src/modules/api/auth.js
@@ -14,8 +14,10 @@ import 'whatwg-fetch';
 // Import fetch helper functions.
 import { parseJSON } from './fetch-utils';
 import validator from '../utils/validator'
+
+// Oauth 2 variables
 const API_URL = process.env.REACT_APP_API_URL;
-const API_AUTHENTICATE_URL = API_URL + '/api/login';
+const API_CLIENT_ID = '1fbceff0aee0941934c81ba29cb7454c';
 
 /**
  * Authenticate a user
@@ -25,16 +27,17 @@ const API_AUTHENTICATE_URL = API_URL + '/api/login';
  */
 export function authenticate(username, password) {
   return new Promise((resolve, reject) => {
-    validator.validate({ username: username, password: password }).then((data) => {
-        fetch(API_AUTHENTICATE_URL, {
+    validator.validate({ username: username, password: password })
+      .then((data) => {
+        fetch(API_URL + '/oauth/tokens', {
           method: 'POST',
           headers: {
             "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
-            //"Authorization": 'Basic MWZiY2VmZjBhZWUwOTQxOTM0YzgxYmEyOWNiNzQ1NGM6YjAyNDZlNjhiZGMzZWI3MzhkNGU4NTcyYzFjOWZmODQ1OThlNjdiZDUzMzc1OWNjZmU0MGQwZmIxOGNhMTQ1Nw=='
           },
           body: 'username=' + encodeURIComponent(data.username) +
             '&password=' + encodeURIComponent(data.password) +
-            '&grant_type=client_credentials'
+            '&client_id=' + encodeURIComponent(API_CLIENT_ID) +
+            '&grant_type=' + encodeURIComponent('password')
         }).then(parseJSON)
           .then((response) => {
             if (response.ok) {
@@ -42,8 +45,61 @@ export function authenticate(username, password) {
             }
             return reject(response.json);
           });
-    }).catch((error) => {      // Validation errors
-      reject(error);
-    });
+      })
+      .catch((error) => {      // Validation errors
+        reject(error);
+      });
   });
+}
+
+/**
+ * Get profile information about the logged in user
+ * @param token
+ * @returns {Promise<any>}
+ */
+export function getMe(token) {
+  return new Promise((resolve, reject) => {
+    fetch(API_URL + '/oauth/me', {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Authorization": "Bearer " + token
+      }
+    }).then(parseJSON)
+      .then((response) => {
+        if (response.ok) {
+          return resolve(response.json);
+        }
+        return reject(response.json);
+      });
+
+  })
+}
+
+/**
+ * Refresh an access token
+ * @param refresh_token
+ * @returns {Promise<any>}
+ */
+export function refreshToken(refresh_token) {
+  return new Promise((resolve, reject) => {
+    fetch(API_URL + '/oauth/tokens', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+        //"Authorization": "Bearer " + token
+      },
+      body: 'refresh_token=' + encodeURIComponent(refresh_token) +
+      '&client_id=' + encodeURIComponent(API_CLIENT_ID) +
+      '&grant_type=' + encodeURIComponent('refresh_token')
+
+    }).then(parseJSON)
+      .then((response) => {
+        if (response.ok) {
+          return resolve(response.json);
+        }
+        return reject(response.json);
+      });
+
+  })
 }
