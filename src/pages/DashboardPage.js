9845314eeac7460157fa4a4f6722aed47ff08312
HenrikGr <henrik.gronvall@bredband.net>
2018-03-04 21:35:00 +0100

    Commit and push updates

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -5,9 +5,7 @@
  * @version:  0.0.1
  * @link:
  * @copyright:  Copyright (c) 2017 HGC AB
- *
  * @license: The MIT License (MIT)
- * @link: https://opensource.org/licenses/MIT
  */
 import React from 'react';
 //import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom'
