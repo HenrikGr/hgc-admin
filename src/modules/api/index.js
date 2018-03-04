4b499c8a9db04adc6ebd79e4e8fdb4d96e543441
HenrikGr <henrik.gronvall@bredband.net>
2018-03-04 21:39:13 +0100

    Commit and push updates

--- src/modules/api/index.js
+++ src/modules/api/index.js
@@ -1,14 +1,25 @@
+/**
+ * Description: API module
+ *
+ * @author:   Henrik Grönvall
+ * @version:  0.0.1
+ * @copyright:  Copyright (c) 2017 HGC AB
+ * @license: The MIT License (MIT)
+ * @link: https://opensource.org/licenses/MIT
+ */
 
-import { authenticate } from "./auth";
+import { authenticate, getMe, refreshToken } from "./auth";
 import { createClient, getClientByName } from "./client";
 import { createUser } from "./users";
 
+// Export the whole API
 const API = {
   authenticate,
+  getMe,
+  refreshToken,
   createUser,
   createClient,
   getClientByName,
 };
-
 
 export default API;
