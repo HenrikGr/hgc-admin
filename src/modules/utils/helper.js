6864a5decbba394a551948a5c2095d7899c43c4a
HenrikGr <henrik.gronvall@bredband.net>
2018-03-11 23:28:44 +0100

    Commit and push updates

--- src/modules/utils/helper.js
+++ src/modules/utils/helper.js
@@ -3,7 +3,6 @@
  *
  * @author:   Henrik Grönvall
  * @version:  0.0.1
- * @link:
  * @copyright:  Copyright (c) 2017 HGC AB
  * @license: The MIT License (MIT)
  */
@@ -13,6 +12,33 @@
  * @param obj
  * @returns {boolean}
  */
-export function isEmpty( obj ) {
+export function isEmpty(obj) {
   return Object.keys(obj).length === 0 && obj.constructor === Object;
 }
+
+/**
+ * Format hours, minutes and seconds to a readable string
+ * @param hours
+ * @param minutes
+ * @param seconds
+ * @returns {string}
+ */
+export const formatExpiresIn = ({ hours, minutes, seconds }) => {
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  return hours + ":" + minutes + ":" + seconds;
+};
+
+/**
+ * Parse number of seconds into hour, minutes and seconds
+ * @param seconds
+ * @returns {{hours: number, minutes: number, seconds: number}}
+ */
+export const parseHHMMSS = seconds => {
+  return {
+    hours: Math.floor(seconds / 3600) % 24,
+    minutes: Math.floor(seconds / 60) | 0,
+    seconds: (seconds % 60) | 0
+  };
+};
