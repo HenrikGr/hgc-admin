0c3e3ddc1c90097e8ebf721870ad67c623d18935
HenrikGr <henrik.gronvall@bredband.net>
2018-03-11 23:32:33 +0100

    Commit and push updates

---
+++ src/modules/state/middleware/thunk.js
@@ -0,0 +1,28 @@
+/**
+ * Description
+ *
+ * @author:   Henrik Grönvall
+ * @version:  0.0.1
+ * @copyright:  Copyright (c) 2017 HGC AB
+ * @license: The MIT License (MIT)
+ */
+
+/**
+ *
+ * @param extraArgument
+ * @returns {function({dispatch?: *, getState?: *}): function(*): function(*=)}
+ */
+function createThunkMiddleware(extraArgument) {
+  return ({ dispatch, getState }) => next => action => {
+    if (typeof action === "function") {
+      return action(dispatch, getState, extraArgument);
+    }
+
+    return next(action);
+  };
+}
+
+const thunk = createThunkMiddleware();
+thunk.withExtraArgument = createThunkMiddleware;
+
+export default thunk;
