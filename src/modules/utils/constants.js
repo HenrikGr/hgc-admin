e0d21f80c56d2881a855ac1ec24f38525dda653f
HenrikGr <henrik.gronvall@bredband.net>
2018-03-11 23:28:26 +0100

    Commit and push updates

--- src/modules/utils/constants.js
+++ src/modules/utils/constants.js
@@ -1,11 +1,10 @@
-/*
+/**
  * Description: Module containing constants for the application
  *
- * Author:  Henrik
- * File:    
- * Version: 0.0.1
- *
- * Created: 2016-10-15
+ * @author:   Henrik Grönvall
+ * @version:  0.0.1
+ * @copyright:  Copyright (c) 2017 HGC AB
+ * @license: The MIT License (MIT)
  */
 
 /**
@@ -15,12 +14,11 @@
 // 6 to 20 characters which contain at least one numeric digit, one uppercase and one lowercase letter
 const PASS_6TO20_ONE_UPP_LOW_NUM = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
 // 7 to 15 characters which contain only characters, numeric digits, underscore and first character must be a letter
-const PASS_7TO15_FIRST_UPP_ONLY_CHR_NUM_UND =  /^[A-Za-z]\w{7,14}$/;
+const PASS_7TO15_FIRST_UPP_ONLY_CHR_NUM_UND = /^[A-Za-z]\w{7,14}$/;
 // 7 to 15 characters which contain at least one numeric digit and a special character
-const PASS_7TO15_LEAST_ONE_NUM_SPEC =  /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
+const PASS_7TO15_LEAST_ONE_NUM_SPEC = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
 // 8 to 15 characters which contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character
-const PASS_8TO15_LEAST_ONE_LOW_UPP_NUM_SPEC =  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
-
+const PASS_8TO15_LEAST_ONE_LOW_UPP_NUM_SPEC = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
 
 /**
  * Text regular expression matching rules
@@ -28,7 +26,6 @@ const PASS_8TO15_LEAST_ONE_LOW_UPP_NUM_SPEC =  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(
  */
 const ONLY_LETTERS = /^[A-Za-z]+$/;
 const ONLY_NUMBERS = /^[0-9]+$/;
-
 
 /**
  * Export constants
@@ -40,4 +37,4 @@ export {
   PASS_8TO15_LEAST_ONE_LOW_UPP_NUM_SPEC,
   ONLY_LETTERS,
   ONLY_NUMBERS
-}
+};
