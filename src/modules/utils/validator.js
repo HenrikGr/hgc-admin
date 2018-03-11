7abd84a170dfa6c85b89d76c2098de32c92dffe9
HenrikGr <henrik.gronvall@bredband.net>
2018-03-11 23:29:27 +0100

    Commit and push updates

--- src/modules/utils/validator.js
+++ src/modules/utils/validator.js
@@ -17,24 +17,23 @@
  * @version:  0.0.1
  * @copyright:  Copyright (c) 2017 HGC AB
  * @license: The MIT License (MIT)
- * @link: https://opensource.org/licenses/MIT
  */
 
 // Import constants
-import { PASS_6TO20_ONE_UPP_LOW_NUM } from './constants';
+import { PASS_6TO20_ONE_UPP_LOW_NUM } from "./constants";
 
 // Import application error module
-import AppError from '../api/error';
+import AppError from "../api/error";
 
 /**
  * Validator object
  */
 const validator = {
-  types:{},
-  config:{},
+  types: {},
+  config: {},
 
-  validate( data ) {
-    return new Promise( ( resolve, reject ) => {
+  validate(data) {
+    return new Promise((resolve, reject) => {
       let key;
       let isValid = true;
 
@@ -44,18 +43,18 @@ const validator = {
 
       for (key in data) {
         if (data.hasOwnProperty(key)) {
-          let type = this.config[key];      // Get validation type
-          let checker = this.types[type];   // Get the validation function for this type
-          
+          let type = this.config[key]; // Get validation type
+          let checker = this.types[type]; // Get the validation function for this type
+
           if (!type) {
             continue; // no need to validate
           }
-      
+
           // No validation function for this type
           if (!checker) {
-            reject(new AppError('Error in validation, no checker for ' + type));
+            reject(new AppError("Error in validation, no checker for " + type));
           }
-      
+
           // Run the validation function
           if (!checker.validate(data[key])) {
             isValid = false;
@@ -63,57 +62,61 @@ const validator = {
           }
         }
       }
-  
+
       if (!isValid) {
         reject(result.error);
       } else {
         resolve(data);
       }
-
     });
   }
 };
 
 // checks against password rules
 validator.types.isPassword = {
-  validate:function (value) {
+  validate: function(value) {
     return value.match(PASS_6TO20_ONE_UPP_LOW_NUM);
   },
-  instructions:"Value must be 6-20 long and at least one numeric, one uppercase and one lowercase"
+  instructions:
+    "Value must be 6-20 long and at least one numeric, one uppercase and one lowercase"
 };
 
 // checks for non-empty values
 validator.types.isNonEmpty = {
-  validate:function (value) {
+  validate: function(value) {
     return value !== "";
   },
-  instructions:"Value cannot be empty"
+  instructions: "Value cannot be empty"
 };
 
 // checks if a value is a number
 validator.types.isNumber = {
-  validate:function (value) {
+  validate: function(value) {
     return !isNaN(value);
   },
-  instructions:"Value can only be a valid number, e.g. 1, 3.14 or 2010"
+  instructions: "Value can only be a valid number, e.g. 1, 3.14 or 2010"
 };
 
 // checks if the value contains only letters and numbers
 validator.types.isAlphaNum = {
-  validate:function (value) {
+  validate: function(value) {
     return !/[^a-z0-9]/i.test(value);
   },
-  instructions:"Value can only contain characters and numbers, no special symbols"
+  instructions:
+    "Value can only contain characters and numbers, no special symbols"
 };
 
 // Set up validation types to input field names.
 validator.config = {
-  username:'isNonEmpty',
-  password:'isPassword',
-  name:'isNonEmpty',
-  user: 'isNonEmpty',
+  username: "isNonEmpty",
+  password: "isPassword",
+  name: "isNonEmpty",
+  user: "isNonEmpty",
+  email: "isNonEmpty",
+  firstName: "isNonEmpty",
+  lastName: "isNonEmpty",
+  phone: "isNonEmpty"
 };
-
 
 /**
  * Export the validator object
