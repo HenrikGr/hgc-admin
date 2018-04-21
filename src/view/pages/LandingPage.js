c098f98a843a3005cf9584f22f2a83f1c3d27050
HenrikGr <henrik.gronvall@bredband.net>
2018-04-21 22:22:05 +0200

    Commit and push updates

--- src/view/pages/LandingPage.js
+++ src/view/pages/LandingPage.js
@@ -1,14 +1,20 @@
 /**
- * Description:
+ * Description: Landing page component
+ *
+ * Container component for the landing page
+ *
+ * Container components receives state trough props, supply
+ * changes to the the presentation layer and update states
+ * to the store.
  *
  * @author:   Henrik Grönvall
  * @version:  0.0.1
  * @copyright:  Copyright (c) 2017 HGC AB
  * @license: The MIT License (MIT)
  */
+
 import React from 'react';
 //import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom'
-//import store from '../store'
 
 
 const LandingPage = ({...rest}) => (
