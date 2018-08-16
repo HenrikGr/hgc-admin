761df20ef62097e8c225faad5a1d6724da1854c3
HenrikGr <henrik.gronvall@bredband.net>
2018-08-16 02:32:26 +0200

    Refactored

--- src/view/layout/AppLayout.js
+++ src/view/layout/AppLayout.js
@@ -10,7 +10,6 @@
 // React & React Router
 import React from "react";
 import PropTypes from "prop-types";
-import { connect } from "react-redux";
 
 // Material ui
 import { withStyles } from "@material-ui/core/styles";
@@ -19,11 +18,8 @@ import { withStyles } from "@material-ui/core/styles";
 import NavigationBar from './NavigationBar';
 import Main from './Main';
 
-// Session actions
-import sessionActions from "../../store/actions/SessionAction";
-
 // Component styles
-const styles = {
+const styles = theme => ({
   root: {
     flexGrow: 1,
     zIndex: 1,
@@ -31,67 +27,34 @@ const styles = {
     position: "relative",
     display: "flex"
   },
-};
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    marginTop: "56px",
+    [theme.breakpoints.up("sm")]: {
+      marginTop: "64px"
+    }
+  }
+});
 
 /**
  * Application layout component
  * @param {object} classes - style object used by material-ui withStyle HOC
- * @param {object} token - token if user is authenticated
- * @param {function} removeSession - function to remove session with token
- * @param {function} refreshSession - used to refresh token when expired
  * @returns {*}
  * @constructor
  */
-function AppLayout({ classes, token, removeSession, refreshSession }) {
+function AppLayout({ classes }) {
   return(
     <div className={classes.root}>
-      <NavigationBar
-        token={ token }
-        removeSession={ removeSession }
-        refreshSession={ refreshSession }
-      />
+      <NavigationBar/>
       <Main/>
     </div>
   )
 }
 
-/**
- * Props API
- * @type {{classes: *, token: *, removeSession: *, refreshSession: *}}
- */
 AppLayout.propTypes = {
   classes: PropTypes.object.isRequired,
-  token: PropTypes.object.isRequired,
-  removeSession: PropTypes.func.isRequired,
-  refreshSession: PropTypes.func.isRequired,
-};
-
-/**
- * Map token state to props
- * @param {object} state - global state
- * @returns {{token: (defaults.session.token|{})}}
- */
-const mapStateToProps = state => {
-  return {
-    token: state.session.token,
-  };
-};
-
-/**
- * Map session actions to props
- * @param {function} dispatch - function used to dispatch actions
- * @returns {{removeSession: removeSession, refreshSession: refreshSession}}
- */
-const mapDispatchToProps = dispatch => {
-  return {
-    removeSession: () => {
-      dispatch(sessionActions.removeSession());
-    },
-    refreshSession: () => {
-      dispatch(sessionActions.refreshSession());
-    }
-  };
 };
 
 // Inject state, actions and styles to component
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(AppLayout));
+export default withStyles(styles)(AppLayout);
