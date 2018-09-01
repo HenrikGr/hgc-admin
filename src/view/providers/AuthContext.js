739c883011a246febe7a2e3814c7f9413dc35948
HenrikGr <henrik.gronvall@bredband.net>
2018-09-01 02:11:58 +0200

    Initial commit

---
+++ src/view/providers/AuthContext.js
@@ -0,0 +1,19 @@
+/**
+ * Description: AuthContext
+ *
+ * @author:   Henrik Grönvall
+ * @version:  0.0.1
+ * @copyright:  Copyright (c) 2017 HGC AB
+ * @license: The MIT License (MIT)
+ */
+
+// react
+import React from "react";
+
+/**
+ * Create the AuthContext
+ */
+const AuthContext = React.createContext(null);
+
+// Export auth context
+export default AuthContext;
