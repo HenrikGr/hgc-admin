3ce67bb9d3b9de229faf9e0e824d004addc5c03f
HenrikGr <henrik.gronvall@bredband.net>
2018-09-05 01:51:23 +0200

    Prettified and documented

--- src/view/components/forms/ProfileForm.js
+++ src/view/components/forms/ProfileForm.js
@@ -1,83 +1,83 @@
 /**
- * Description: ProfileForm component
- *
- * The ProfileForm component is a controlled component
- * and thus should not hold any state data.
- *
+ * @prettier
+ * @description: ProfileForm controlled component
  * @author:   Henrik Grönvall
  * @version:  0.0.1
  * @copyright:  Copyright (c) 2017 HGC AB
  * @license: The MIT License (MIT)
  */
+import React from 'react'
+import PropTypes from 'prop-types'
+import Button from '@material-ui/core/Button'
+import FormLabel from '@material-ui/core/FormLabel'
+import { withStyles } from '@material-ui/core/styles'
+import TextField from '../fields/TextField'
 
-// react
-import React from "react";
-import PropTypes from "prop-types";
-
-// material-ui
-import Button from "@material-ui/core/Button";
-import FormLabel from "@material-ui/core/FormLabel";
-import { withStyles } from "@material-ui/core/styles";
-
-// custom components
-import TextField from '../fields/TextField';
-
-// CSS
-import styles from '../../styles/ProfileForm'
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  progress: {
+    flexGrow: 1,
+    height: '5px'
+  },
+  formWrapper: {
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    marginTop: '80px',
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '552px'
+    }
+  },
+  formControl: {
+    margin: theme.spacing.unit
+  },
+  button: {
+    margin: theme.spacing.unit
+  }
+})
 
 /**
  * ProfileForm component
  * @param classes
  * @param formLabel
  * @param entity
- * @param isFetching
+ * @param disableSubmit
  * @param onChange
  * @param onSubmit
  * @returns {*}
  * @constructor
  */
-function ProfileForm({classes, formLabel, entity, isFetching, onChange, onSubmit }) {
-  return(
+function ProfileForm({ classes, formLabel, entity, disableSubmit, onChange, onSubmit }) {
+  return (
     <div className={classes.root}>
       <div className={classes.formWrapper}>
-        <form className={classes.form} onSubmit={ onSubmit } >
-
-          <FormLabel component="legend">
-            {formLabel}
-          </FormLabel>
-
+        <form className={classes.form} onSubmit={onSubmit}>
+          <FormLabel component="legend">{formLabel}</FormLabel>
           <TextField
             id="email"
             type="email"
             label="Email"
-            value={ entity.email }
-            onChange={ onChange }
+            value={entity.email}
+            onChange={onChange}
           />
-
           <TextField
             id="firstName"
             label="First name"
-            value={ entity.firstName }
-            onChange={ onChange }
+            value={entity.firstName}
+            onChange={onChange}
           />
-
-          <TextField
-            id="lastName"
-            label="Last name"
-            value={ entity.lastName }
-            onChange={ onChange }
-          />
-
-          <TextField
-            id="phone"
-            type="tel"
-            label="Phone"
-            value={ entity.phone }
-            onChange={ onChange }
-          />
-
+          <TextField id="lastName" label="Last name" value={entity.lastName} onChange={onChange} />
+          <TextField id="phone" type="tel" label="Phone" value={entity.phone} onChange={onChange} />
           <Button
-            disabled={isFetching}
+            disabled={disableSubmit}
             type="submit"
             variant="raised"
             color="primary"
@@ -92,16 +92,34 @@ function ProfileForm({classes, formLabel, entity, isFetching, onChange, onSubmit
 }
 
 /**
- * Props type checking
+ * Property type checks
  */
 ProfileForm.propTypes = {
+  /**
+   * Object used to extends style
+   */
   classes: PropTypes.object.isRequired,
+  /**
+   * Form label text
+   */
   formLabel: PropTypes.string.isRequired,
+  /**
+   * Data entity
+   */
   entity: PropTypes.object.isRequired,
-  isFetching: PropTypes.bool.isRequired,
+  /**
+   * Boolean indicating if submit btn should be disabled
+   */
+  disableSubmit: PropTypes.bool.isRequired,
+  /**
+   * Callback for input changes
+   */
   onChange: PropTypes.func.isRequired,
+  /**
+   * Callback for submitting the form
+   */
   onSubmit: PropTypes.func.isRequired
-};
+}
 
 // Inject styles into the form
-export default withStyles(styles)(ProfileForm);
+export default withStyles(styles)(ProfileForm)
