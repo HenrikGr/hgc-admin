455e1592054d04bc950660cdfecdd04d4305e330
HenrikGr <henrik.gronvall@bredband.net>
2018-09-05 19:24:34 +0200

    Prettified and documented

--- src/view/components/notification/Notification.js
+++ src/view/components/notification/Notification.js
@@ -1,83 +1,86 @@
 /**
- * Description: Notification container component
- *
+ * @prettier
+ * @description: Notification container component
  * @author:   Henrik Grönvall
  * @version:  0.0.1
  * @copyright:  Copyright (c) 2017 HGC AB
  * @license: The MIT License (MIT)
  */
-
-// react
-import React from 'react';
-import PropTypes from 'prop-types';
-
-// material-ui
-import Snackbar from '@material-ui/core/Snackbar';
-
-// custom component
+import React from 'react'
+import PropTypes from 'prop-types'
+import Snackbar from '@material-ui/core/Snackbar'
 import NotificationContent from './NotificationContent'
 
 /**
- * Notification
+ * Notification component
+ * @class
+ * @public
  */
 class Notification extends React.Component {
   /**
-   * Props API
+   * Property type checks
+   * @type {Object}
    */
   static propTypes = {
     /**
      * Vertical anchor position
+     * @public
      */
     vertical: PropTypes.string.isRequired,
     /**
      * Horizontal anchor position
+     * @public
      */
     horizontal: PropTypes.string.isRequired,
     /**
      * Delay before auto hide
+     * @public
      */
     autoHideDuration: PropTypes.number.isRequired,
     /**
      * Default message for unexpected errors
+     * @public
      */
     variant: PropTypes.oneOf(['success', 'warning', 'error', 'info']),
     /**
      * messages object
+     * @public
      */
     messages: PropTypes.object,
     /**
      * Callback to reset messages
+     * @callback
      */
-    onResetMessages: PropTypes.func,
-  };
+    onResetMessages: PropTypes.func
+  }
 
   static defaultProps = {
     vertical: 'top',
     horizontal: 'center',
     autoHideDuration: 1000 * 6, // 6 seconds
     variant: 'error',
-    messages: {},
-  };
+    messages: {}
+  }
 
   /**
    * Initial state
    * @type {{open: boolean}}
    */
   state = {
-    open: false,
-  };
+    open: false
+  }
 
   /**
-   * Ensure the snackbar opens when new error message arrived
+   * Ensure the snackbar opens when new message arrived
    * @param prevProps
    * @param prevState
    */
   componentDidUpdate(prevProps, prevState) {
     if (this.props.messages !== prevProps.messages) {
       if (this.props.messages.message) {
-        this.setState({open: true})
+        this.setState({ open: true })
       } else {
-        this.setState({open: false})
+        this.setState({ open: false })
       }
     }
   }
@@ -89,33 +92,35 @@ class Notification extends React.Component {
    */
   handleClose = (event, reason) => {
     if (reason === 'clickaway') {
-      return;
+      return
     }
-    // Reset error object
-    this.props.onResetMessages();
-    this.setState({ open: false });
-  };
+
+    this.setState({ open: false })
+
+    if (this.props.onResetMessages) {
+      this.props.onResetMessages()
+    }
+  }
 
   render() {
-    return(
+    return (
       <Snackbar
         anchorOrigin={{
           vertical: this.props.vertical,
-          horizontal: this.props.horizontal,
+          horizontal: this.props.horizontal
         }}
-        open={ this.state.open }
-        autoHideDuration={ this.props.autoHideDuration }
-        onClose={ this.handleClose }
+        open={this.state.open}
+        autoHideDuration={this.props.autoHideDuration}
+        onClose={this.handleClose}
       >
         <NotificationContent
-          variant={ this.props.variant }
-          messages={ this.props.messages }
-          onClose={ this.handleClose }
+          variant={this.props.variant}
+          messages={this.props.messages}
+          onClose={this.handleClose}
         />
       </Snackbar>
-
     )
   }
 }
 
-export default Notification;
+export default Notification
--- src/view/components/notification/NotificationContent.js
+++ src/view/components/notification/NotificationContent.js
@@ -1,83 +1,86 @@
 /**
- * Description: Notification container component
- *
+ * @prettier
+ * @description: Notification content component that renders the notification
  * @author:   Henrik Grönvall
  * @version:  0.0.1
  * @copyright:  Copyright (c) 2017 HGC AB
  * @license: The MIT License (MIT)
  */
-
-// react
-import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-
-// material-ui
-import CheckCircleIcon from '@material-ui/icons/CheckCircle';
-import ErrorIcon from '@material-ui/icons/Error';
-import InfoIcon from '@material-ui/icons/Info';
-import CloseIcon from '@material-ui/icons/Close';
-import green from '@material-ui/core/colors/green';
-import amber from '@material-ui/core/colors/amber';
-import IconButton from '@material-ui/core/IconButton';
-import SnackbarContent from '@material-ui/core/SnackbarContent';
-import WarningIcon from '@material-ui/icons/Warning';
-import { withStyles } from '@material-ui/core/styles';
+import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import CheckCircleIcon from '@material-ui/icons/CheckCircle'
+import ErrorIcon from '@material-ui/icons/Error'
+import InfoIcon from '@material-ui/icons/Info'
+import CloseIcon from '@material-ui/icons/Close'
+import green from '@material-ui/core/colors/green'
+import amber from '@material-ui/core/colors/amber'
+import IconButton from '@material-ui/core/IconButton'
+import SnackbarContent from '@material-ui/core/SnackbarContent'
+import WarningIcon from '@material-ui/icons/Warning'
+import { withStyles } from '@material-ui/core/styles'
 
 // helper
-import { capitalizeFirstLetter } from "../../../utils/stringHelper";
+import { capitalizeFirstLetter } from '../../../utils/stringHelper'
 
 // Variant icons
 const variantIcon = {
   success: CheckCircleIcon,
   warning: WarningIcon,
   error: ErrorIcon,
-  info: InfoIcon,
-};
+  info: InfoIcon
+}
 
 const styles = theme => ({
   success: {
     backgroundColor: green[600],
+    display: 'flex',
+    flexDirection: 'column'
   },
   error: {
     backgroundColor: theme.palette.error.dark,
+    display: 'flex',
+    flexDirection: 'column'
   },
   info: {
     backgroundColor: theme.palette.primary.dark,
+    display: 'flex',
+    flexDirection: 'column'
   },
   warning: {
     backgroundColor: amber[700],
+    display: 'flex',
+    flexDirection: 'column'
   },
   icon: {
-    fontSize: 20,
+    fontSize: 20
   },
   iconVariant: {
     opacity: 0.9,
-    marginRight: theme.spacing.unit,
+    marginRight: theme.spacing.unit
   },
   message: {
     display: 'flex',
-    //alignItems: 'center',
-  },
-});
+    alignItems: 'flex-start'
+  }
+})
 
 /**
- * Helper function to transform error object into HTML
+ * Helper function to transform message object into HTML
  * @param messages
  * @returns {string}
  */
 const getMessage = messages => {
-  let msg = '';
+  let msg = ''
   for (const [key, value] of Object.entries(messages)) {
     if (key === 'message') {
-      msg = '<div>' + value + '</div></br>';
+      msg = '<div>' + value + '</div></br>'
     } else {
-      msg = msg + '<div>' + capitalizeFirstLetter(key) + ' ' + value +'</div>';
+      msg = msg + '<div>' + capitalizeFirstLetter(key) + ' ' + value + '</div>'
     }
   }
-  return msg;
-};
-
+  return msg
+}
 
 /**
  * NotificationContent component
@@ -90,20 +93,15 @@ const getMessage = messages => {
  * @constructor
  */
 function NotificationContent({ classes, messages, onClose, variant, ...other }) {
-  const Icon = variantIcon[variant];
-
+  const Icon = variantIcon[variant]
   return (
     <SnackbarContent
       className={classNames(classes[variant])}
       aria-describedby="client-snackbar"
       message={
-        <div id="client-snackbar" className={ classes.message }>
+        <div id="client-snackbar" className={classes.message}>
           <Icon className={classNames(classes.icon, classes.iconVariant)} />
-          <div
-            id="message-id"
-            dangerouslySetInnerHTML={{ __html: getMessage(messages) }}
-          >
-          </div>
+          <div id="message-id" dangerouslySetInnerHTML={{ __html: getMessage(messages) }} />
         </div>
       }
       action={[
@@ -111,22 +109,41 @@ function NotificationContent({ classes, messages, onClose, variant, ...other }) 
           key="close"
           aria-label="Close"
           color="inherit"
-          className={ classes.close }
-          onClick={ onClose }
+          className={classes.close}
+          onClick={onClose}
         >
-          <CloseIcon className={ classes.icon } />
-        </IconButton>,
+          <CloseIcon className={classes.icon} />
+        </IconButton>
       ]}
       {...other}
     />
-  );
+  )
 }
 
+/**
+ * Property type checks
+ * @type {Object}
+ */
 NotificationContent.propTypes = {
+  /**
+   * Classes, can be used to override css styles
+   */
   classes: PropTypes.object.isRequired,
+  /**
+   * Message object to be displayed
+   * @public
+   */
   messages: PropTypes.object,
-  onClose: PropTypes.func,
+  /**
+   * Variant of notification
+   * @public
+   */
   variant: PropTypes.oneOf(['success', 'warning', 'error', 'info']).isRequired,
-};
+  /**
+   * Callback to close the content
+   * @callback
+   */
+  onClose: PropTypes.func
+}
 
-export default withStyles(styles)(NotificationContent);
+export default withStyles(styles)(NotificationContent)
