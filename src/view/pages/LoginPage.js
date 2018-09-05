7d98b3511103f353e02df76ab8ec4f8cde1fec88
HenrikGr <henrik.gronvall@bredband.net>
2018-09-05 02:12:11 +0200

    Prettified and documented

--- src/view/pages/LoginPage.js
+++ src/view/pages/LoginPage.js
@@ -13,10 +13,8 @@ import { Redirect } from 'react-router'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 
-// material-ui
-import Grid from '@material-ui/core/Grid'
-
 // Presentation layer
+import Grid from '@material-ui/core/Grid'
 import LoginForm from '../components/forms/LoginForm'
 import Notification from '../components/notification/Notification'
 import LinearProgressbar from '../components/progress/LinearProgressbar'
@@ -35,42 +33,30 @@ class LoginPage extends React.PureComponent {
    * Property type checks
    */
   static propTypes = {
-
     /**
      * Flag indicating if user successfully logged in
-     * Is used to redirect
-     * @ignore
      */
     isAuth: PropTypes.bool.isRequired,
-
     /**
-     * Flag indicating fetch actions
-     * @ignore
+     * Flag indicating fetch state
      */
     isFetching: PropTypes.bool.isRequired,
-
     /**
      * error object
-     * @ignore
      */
     error: PropTypes.object.isRequired,
-
     /**
-     * Function to log in
-     * @ignore
+     * Callback mapped to user action
      */
     logIn: PropTypes.func.isRequired,
-
     /**
-     * Function to reset error
-     * @ignore
+     * Callback mapped to user action
      */
     resetError: PropTypes.func.isRequired
   }
 
   /**
    * Sets initial component state
-   * @private
    */
   state = {
     username: '',
@@ -79,22 +65,37 @@ class LoginPage extends React.PureComponent {
     redirectToReferrer: false
   }
 
+  /**
+   * Reset possible errors on un mount
+   */
   componentWillUnmount() {
     if (!isEmpty(this.props.error)) {
       this.props.resetError()
     }
   }
 
+  /**
+   * Event handler to deal with input values from the form
+   * @param prop
+   * @returns {Function}
+   */
   handleChange = prop => event => {
     this.setState({ [prop]: event.target.value })
   }
 
+  /**
+   * Event handler for form submit
+   * @param event
+   */
   handleSubmit = event => {
     const { username, password } = this.state
     event.preventDefault()
     this.props.logIn({ username, password })
   }
 
+  /**
+   * Event handler for toggle password visibility
+   */
   handleShowPassword = () => {
     this.setState(prevState => {
       return {
@@ -104,6 +105,9 @@ class LoginPage extends React.PureComponent {
     })
   }
 
+  /**
+   * Event handler to reset validation errors
+   */
   handleReset = () => {
     if (!isEmpty(this.props.error)) {
       this.props.resetError()
@@ -123,14 +127,12 @@ class LoginPage extends React.PureComponent {
       <Grid container spacing={0}>
         <Grid item xs={12}>
           <Notification variant="error" messages={error} onResetMessages={this.handleReset} />
-
           <LinearProgressbar isFetching={isFetching} />
-
           <LoginForm
             formLabel="Log in"
             entity={{ username, password }}
             showPassword={showPassword}
-            isFetching={isFetching}
+            disableSubmit={isFetching}
             onSubmit={this.handleSubmit}
             onChange={this.handleChange}
             onShowPassword={this.handleShowPassword}
@@ -142,18 +144,20 @@ class LoginPage extends React.PureComponent {
 }
 
 /**
- * Map global state to props
+ * Map state to props
  * @param state
+ * @returns {{isAuth: boolean, isFetching: boolean, error: (defaults.user.error|{})}}
  */
 const mapStateToProps = state => ({
-  isAuth: state.session.isAuth,
-  isFetching: state.session.isFetching,
-  error: state.session.error
+  isAuth: state.user.isAuth,
+  isFetching: state.user.isFetching,
+  error: state.user.error
 })
 
 /**
- * Map actions to props
+ * Map user actions to props
  * @param dispatch
+ * @returns {{logIn: logIn, resetError: resetError}}
  */
 const mapDispatchToProps = dispatch => {
   return {
@@ -166,7 +170,7 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-// Inject state and action creators to the presentation layer
+// Connect mapped state and user actions to component properties
 export default connect(
   mapStateToProps,
   mapDispatchToProps
