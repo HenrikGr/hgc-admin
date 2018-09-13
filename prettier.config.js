707762f7a1ab81929095135ba72a1505ef3c5b72
HenrikGr <henrik.gronvall@bredband.net>
2018-09-13 23:50:18 +0200

    Small utils

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+/**
+ * @description: Prettier configuration file
+ * @author:   Henrik Grönvall
+ * @version:  0.0.1
+ * @copyright:  Copyright (c) 2017 HGC AB
+ * @license: The MIT License (MIT)
+ */
+
+module.exports = {
+  printWidth: 100,
+  singleQuote: true,
+  semi: false,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  requirePragma: false
+};
