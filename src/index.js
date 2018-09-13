9f3c7bb33ec06bb908354f3e8aed050dadebfe36
HenrikGr <henrik.gronvall@bredband.net>
2018-09-13 05:48:16 +0200

    Prettified

--- src/index.js
+++ src/index.js
@@ -1,32 +1,24 @@
 /**
- * Description: Entry point for the application
- *
+ * @prettier
+ * @description: Entry point for the application
  * @author:   Henrik Grönvall
  * @version:  0.0.1
  * @copyright:  Copyright (c) 2017 HGC AB
  * @license: The MIT License (MIT)
  */
 
-// Polyfills
-//import './utils/polyfills.js';
-
-// Module dependencies
-import React from 'react';
-import { render } from 'react-dom';
-
-// Redux store
+// Must be first import statement in the app.
+// import './utils/polyfills.js'
+import React from 'react'
+import { render } from 'react-dom'
 import store from './store/index'
-
-// Application
-import App from './App';
-//import registerServiceWorker from './registerServiceWorker';
+import App from './App'
+// import registerServiceWorker from './registerServiceWorker';
 
 /**
  * Render the App
  */
-render(
-  <App store={store} />,
-  document.getElementById('root')
-);
+render(<App store={store} />, document.getElementById('root'))
 
+// TODO: Implement the service worker to cache assets
 //registerServiceWorker();
