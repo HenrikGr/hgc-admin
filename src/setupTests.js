fc68e0eba0428938c045198b1c925c7789fdd18c
HenrikGr <henrik.gronvall@bredband.net>
2018-09-13 05:47:05 +0200

    Cleaned up

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,17 +1,17 @@
 /**
- * Set up module for Jest test framework
- *
+ * @prettier
+ * @description: Set up module for Jest test framework
  * @author:   Henrik Grönvall
  * @version:  0.0.1
+ * @copyright:  Copyright (c) 2017 HGC AB
+ * @license: The MIT License (MIT)
  */
-
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
 // Polyfill for cancelAnimationFrame
 global.cancelAnimationFrame = function(callback) {
-  setTimeout(callback, 0);
-};
+  setTimeout(callback, 0)
+}
 
-configure({ adapter: new Adapter() });
-
+configure({ adapter: new Adapter() })
