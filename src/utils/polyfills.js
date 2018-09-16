a21b3e12abee1e885f69ce9da855548dfa22daff
Henrik Grönvall <hgc-ab@outlook.com>
2018-09-16 14:38:29 +0200

    Testing new reinstall of git

--- src/utils/polyfills.js
+++ src/utils/polyfills.js
@@ -1,17 +1,21 @@
 /**
- * Polyfills
- *
- * NPM install core-js to use them
- *
+ * @prettier
+ * @description: Module to configure polyfills to be used
+ * We are not using any extra polyfills but the ones supported by create-react-app framework
+ * @author:   Henrik Grönvall
+ * @version:  0.0.1
+ * @copyright:  Copyright (c) 2017 HGC AB
+ * @license: The MIT License (MIT)
  */
 
-//import 'core-js/es6/map';
-//import 'core-js/es6/set';
-//import 'core-js/fn/number/is-nan';
+/*
+import 'core-js/es6/map'
+import 'core-js/es6/set'
+import 'core-js/fn/number/is-nan'
 
-//import 'core-js/fn/array/find';
-//import 'core-js/fn/array/includes';
+import 'core-js/fn/array/find'
+import 'core-js/fn/array/includes'
 
-//import 'core-js/fn/object/values';
-//import 'core-js/fn/object/entries';
-
+import 'core-js/fn/object/values'
+import 'core-js/fn/object/entries'
+*/
