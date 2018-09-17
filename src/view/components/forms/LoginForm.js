500aa8fa36c2e05896a4839346ce1cb5bf4396bf
Henrik Grönvall <hgc-ab@outlook.com>
2018-09-17 22:05:53 +0200

    Re-styled the component

--- src/view/components/forms/LoginForm.js
+++ src/view/components/forms/LoginForm.js
@@ -6,40 +6,33 @@
  * @copyright:  Copyright (c) 2017 HGC AB
  * @license: The MIT License (MIT)
  */
-import React from "react";
-import PropTypes from "prop-types";
-import Button from "@material-ui/core/Button";
-import FormLabel from "@material-ui/core/FormLabel";
-import { withStyles } from "@material-ui/core/styles";
-import TextField from '../fields/TextField';
-import PasswordField from '../fields/PasswordField';
+import React from 'react'
+import PropTypes from 'prop-types'
+import Button from '@material-ui/core/Button'
+import FormLabel from '@material-ui/core/FormLabel'
+import { withStyles } from '@material-ui/core/styles'
+import TextField from '../fields/TextField'
+import PasswordField from '../fields/PasswordField'
 
 const styles = theme => ({
   root: {
-    display: "flex",
-    flexDirection: "column"
-  },
-  formWrapper: {
-    display: "flex",
-    justifyContent: "center"
+    display: 'flex',
+    justifyContent: 'center',
+    margin: theme.spacing.unit * 3
   },
   form: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    marginTop: "80px",
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "552px"
+    display: 'inline-flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '552px'
     }
-  },
-  formControl: {
-    margin: theme.spacing.unit
   },
   button: {
     margin: theme.spacing.unit
   }
-});
+})
 
 /**
  * LoginForm component
@@ -54,79 +47,92 @@ const styles = theme => ({
  * @returns {*}
  * @constructor
  */
-function LoginForm({ classes, formLabel, entity, showPassword, disableSubmit, onSubmit, onChange, onShowPassword }) {
-  return(
-    <div className={ classes.root }>
-      <div className={ classes.formWrapper }>
-        <form className={ classes.form } onSubmit={ onSubmit }>
-          <FormLabel component="legend">
-            { formLabel }
-          </FormLabel>
-          <TextField
-            id="username"
-            label="User name"
-            required={true}
-            autoComplete="username"
-            value={ entity.username }
-            onChange={ onChange }
-          />
-          <PasswordField
-            id="password"
-            autoComplete="current-password"
-            value={ entity.password }
-            showPassword={ showPassword }
-            onChange={ onChange }
-            onShowPassword={ onShowPassword }
-          />
-          <Button
-            disabled={ disableSubmit }
-            type="submit"
-            variant="raised"
-            color="primary"
-            className={ classes.button }
-          >
-            Login
-          </Button>
-        </form>
-      </div>
+function LoginForm({
+  classes,
+  formLabel,
+  entity,
+  showPassword,
+  disableSubmit,
+  onSubmit,
+  onChange,
+  onShowPassword
+}) {
+  return (
+    <div className={classes.root}>
+      <form className={classes.form} onSubmit={onSubmit}>
+        <FormLabel component="legend">{formLabel}</FormLabel>
+        <TextField
+          id="username"
+          label="User name"
+          required={true}
+          autoComplete="username"
+          value={entity.username}
+          onChange={onChange}
+        />
+        <PasswordField
+          id="password"
+          value={entity.password}
+          showPassword={showPassword}
+          onChange={onChange}
+          onShow={onShowPassword}
+        />
+        <Button
+          disabled={disableSubmit}
+          type="submit"
+          variant="raised"
+          color="primary"
+          className={classes.button}
+        >
+          Login
+        </Button>
+      </form>
     </div>
   )
 }
 
 /**
- * Property type checks
+ * Property type check
+ * @type {Object}
  */
 LoginForm.propTypes = {
   /**
    * Used to style the form
+   * @private
    */
   classes: PropTypes.object.isRequired,
   /**
    * Form label
+   * @public
    */
   formLabel: PropTypes.string.isRequired,
   /**
    * Data entity to be rendered as inputs
+   * @public
    */
   entity: PropTypes.object.isRequired,
   /**
    * Flag indicating password visibility
+   * @public
    */
   showPassword: PropTypes.bool.isRequired,
   /**
    * Boolean indicating if submit btn should be disabled
+   * @public
    */
   disableSubmit: PropTypes.bool.isRequired,
   /**
    * Callback for input changes
+   * @public
    */
   onChange: PropTypes.func.isRequired,
   /**
    * Callback for form submit
+   * @public
    */
   onSubmit: PropTypes.func.isRequired,
   /**
    * Callback to toggle visible password
+   * @public
    */
   onShowPassword: PropTypes.func.isRequired
 }
@@ -136,10 +142,10 @@ LoginForm.propTypes = {
  * @type {{formLabel: string, showPassword: boolean, isFetching: boolean}}
  */
 LoginForm.defaultProps = {
-  formLabel: "Log in",
+  formLabel: 'Log in',
   showPassword: false,
-  isFetching: false,
-};
+  isFetching: false
+}
 
 // Inject styles into the component
-export default withStyles(styles)(LoginForm);
+export default withStyles(styles)(LoginForm)
