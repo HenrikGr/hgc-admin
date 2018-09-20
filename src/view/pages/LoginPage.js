e0b78d96703f2824bb8de7b033e4ca848e7ca552
Henrik Grönvall <hgc-ab@outlook.com>
2018-09-20 02:08:30 +0200

    Add documentation

--- src/view/pages/LoginPage.js
+++ src/view/pages/LoginPage.js
@@ -1,6 +1,11 @@
 /**
  * @prettier
  * @description: Container component for LoginForm component
+ *
+ * The container component is responsible to maintain
+ * - internal state for the presentation layer such as edited credentials, show passwords flags, redirection, etc
+ * - connect to the user global state and action creators to deal with authentication, etc
+ *
  * @author:   Henrik Grönvall
  * @version:  0.0.1
  * @copyright:  Copyright (c) 2017 HGC AB
@@ -30,19 +35,25 @@ class LoginPage extends React.PureComponent {
   static propTypes = {
     /**
      * Flag indicating if user successfully logged in
-     * @public
+     * @private
      */
     isAuthenticated: PropTypes.bool.isRequired,
     /**
-     * Flag indicating fetch state
-     * @public
+     * Flag indicating fetch state, i.e authenticating
+     * @private
      */
-    isFetching: PropTypes.bool.isRequired,
+    isAuthenticating: PropTypes.bool.isRequired,
     /**
      * Callback mapped to user action
-     * @public
+     * @private
      */
-    logIn: PropTypes.func.isRequired
+    authenticate: PropTypes.func.isRequired,
+
+    /**
+     * Location from React Router
+     * @private
+     */
+    location: PropTypes.object
   }
 
   /**
@@ -56,7 +67,7 @@ class LoginPage extends React.PureComponent {
   }
 
   /**
-   * Event handler to deal with input values from the form
+   * Event handler - onChange event on input elements
    * @param prop
    * @returns {Function}
    */
@@ -65,17 +76,17 @@ class LoginPage extends React.PureComponent {
   }
 
   /**
-   * Event handler for form submit
+   * Event handler - onSubmit event on form
    * @param event
    */
   handleSubmit = event => {
     const { username, password } = this.state
     event.preventDefault()
-    this.props.logIn({ username, password })
+    this.props.authenticate({ username, password })
   }
 
   /**
-   * Event handler for toggle password visibility
+   * Event handler - onShowPassword event to toggle password visibility
    */
   handleShowPassword = () => {
     this.setState(prevState => {
@@ -91,11 +102,12 @@ class LoginPage extends React.PureComponent {
    * @returns {*}
    */
   render() {
-    const { isAuthenticated, isFetching } = this.props
+    const { isAuthenticated, isAuthenticating, location } = this.props
     const { username, password, showPassword } = this.state
 
+    // Redirect if authentication success
     if (isAuthenticated) {
-      const { from } = this.props.location.state || { from: { pathname: '/dashboard' } }
+      const { from } = location.state || { from: { pathname: '/dashboard' } }
       return <Redirect to={from} />
     }
 
@@ -104,7 +116,7 @@ class LoginPage extends React.PureComponent {
         formLabel="Log in"
         entity={{ username, password }}
         showPassword={showPassword}
-        disableSubmit={isFetching}
+        disableSubmit={isAuthenticating}
         onSubmit={this.handleSubmit}
         onChange={this.handleChange}
         onShowPassword={this.handleShowPassword}
@@ -114,23 +126,23 @@ class LoginPage extends React.PureComponent {
 }
 
 /**
- * Map state data to props
+ * Map user and fetching state
  * @param state
- * @returns {{isAuth: boolean, isFetching: boolean, error: (defaults.user.error|{})}}
+ * @returns {{isAuthenticated: boolean, isAuthenticating: *}}
  */
 const mapStateToProps = state => ({
   isAuthenticated: state.user.isAuth,
-  isFetching: state.isFetching
+  isAuthenticating: state.isFetching
 })
 
 /**
  * Map user actions to props
  * @param dispatch
- * @returns {{logIn: logIn, resetError: resetError}}
+ * @returns {{authenticate: authenticate}}
  */
 const mapDispatchToProps = dispatch => {
   return {
-    logIn: credentials => {
+    authenticate: credentials => {
       dispatch(userAction.logIn(credentials))
     }
   }
