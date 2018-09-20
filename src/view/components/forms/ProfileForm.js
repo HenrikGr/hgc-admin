2ca87537baab2cf72d8e9f999609d37c17b99ded
Henrik Grönvall <hgc-ab@outlook.com>
2018-09-20 02:09:02 +0200

    Add documentation

--- src/view/components/forms/ProfileForm.js
+++ src/view/components/forms/ProfileForm.js
@@ -16,13 +16,15 @@ import TextField from '../fields/TextField'
 const styles = theme => ({
   root: {
     display: 'flex',
+    flex: '1 0 auto',
     justifyContent: 'center',
-    margin: theme.spacing.unit * 3
+    marginTop: theme.spacing.unit * 8
   },
   form: {
     display: 'inline-flex',
-    flexDirection: 'column',
+    flexFlow: 'column nowrap',
     justifyContent: 'center',
+    margin: theme.spacing.unit * 3,
     width: '100%',
     [theme.breakpoints.up('sm')]: {
       width: '552px'
@@ -49,32 +51,10 @@ function ProfileForm({ classes, formLabel, entity, disableSubmit, onChange, onSu
     <div className={classes.root}>
       <form className={classes.form} onSubmit={onSubmit}>
         <FormLabel component="legend">{formLabel}</FormLabel>
-        <TextField
-          id="email"
-          type="email"
-          label="Email"
-          value={entity.email}
-          onChange={onChange}
-        />
-        <TextField
-          id="firstName"
-          label="First name"
-          value={entity.firstName}
-          onChange={onChange}
-        />
-        <TextField
-          id="lastName"
-          label="Last name"
-          value={entity.lastName}
-          onChange={onChange}
-        />
-        <TextField
-          id="phone"
-          type="tel"
-          label="Phone"
-          value={entity.phone}
-          onChange={onChange}
-        />
+        <TextField id="email" type="email" label="Email" value={entity.email} onChange={onChange} />
+        <TextField id="firstName" label="First name" value={entity.firstName} onChange={onChange} />
+        <TextField id="lastName" label="Last name" value={entity.lastName} onChange={onChange} />
+        <TextField id="phone" type="tel" label="Phone" value={entity.phone} onChange={onChange} />
         <Button
           disabled={disableSubmit}
           type="submit"
@@ -91,30 +71,37 @@ function ProfileForm({ classes, formLabel, entity, disableSubmit, onChange, onSu
 
 /**
  * Property type checks
+ * @type {Object}
  */
 ProfileForm.propTypes = {
   /**
-   * Object used to extends style
+   * Object to create or extend the styles
+   * @private
    */
   classes: PropTypes.object.isRequired,
   /**
-   * Form label text
+   * Form label
+   * @public
    */
   formLabel: PropTypes.string.isRequired,
   /**
-   * Data entity
+   * Data entity to be rendered
+   * @public
    */
   entity: PropTypes.object.isRequired,
   /**
    * Boolean indicating if submit btn should be disabled
+   * @public
    */
   disableSubmit: PropTypes.bool.isRequired,
   /**
    * Callback for input changes
+   * @public
    */
   onChange: PropTypes.func.isRequired,
   /**
-   * Callback for submitting the form
+   * Callback for form submit
+   * @public
    */
   onSubmit: PropTypes.func.isRequired
 }
