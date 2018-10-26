ba037600b89c3e132cc6588cf0561e1ba7617f49
Henrik Grönvall <hgc-ab@outlook.com>
2018-10-26 19:50:30 +0200

    No polyfills used yet

---
+++ src/polyfills/index.js
@@ -0,0 +1,13 @@
+/**
+ * @prettier
+ * @description: Module to configure polyfills to be used
+ *
+ * If polyfills are used it must be the first import statement in the apps entry point.
+ * @see https://github.com/facebook/create-react-app/tree/master/packages/react-app-polyfill
+ * @author:   Henrik Grönvall
+ * @version:  0.0.1
+ * @copyright:  Copyright (c) 2017 HGC AB
+ * @license: The MIT License (MIT)
+ */
+
+//import 'react-app-polyfill/ie11'
