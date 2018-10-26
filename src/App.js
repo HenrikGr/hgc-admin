00644c0608a482925e9e2fc591b20c01efffd4bb
Henrik Grönvall <hgc-ab@outlook.com>
2018-10-26 19:49:11 +0200

    Added self-hosting fonts and async loader components

--- src/App.js
+++ src/App.js
@@ -10,20 +10,28 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { BrowserRouter as Router, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux'
+
+// material-ui
 import withTheme from './view/theme/withTheme'
 
-import LandingPage from './view/pages/LandingPage'
-import LoginFormPage from './view/pages/LoginPage'
-import DashboardPage from './view/pages/DashboardPage'
-import ProfilePage from './view/pages/ProfilePage'
-import UsersPage from './view/pages/UsersPage'
-import ClientPage from './view/pages/ClientPage'
-import NotFoundPage from './view/pages/NotFound'
-
+// Layout components
 import DefaultLayout, { AuthenticatedLayout } from './view/layout/DefaultLayout'
 
 /**
- * App component that provides the global store
+ * Async loading components
+ * @type {AsyncComponent}
+ */
+import asyncComponent from './view/layout/AsyncComponent'
+const AsyncLandingPage = asyncComponent(() => import('./view/pages/LandingPage'))
+const AsyncLoginPage = asyncComponent(() => import('./view/pages/LoginPage'))
+const AsyncDashboardPage = asyncComponent(() => import('./view/pages/DashboardPage'))
+const AsyncProfile = asyncComponent(() => import('./view/pages/ProfilePage'))
+const AsyncUsersPage = asyncComponent(() => import('./view/pages/UsersPage'))
+const AsyncClientPage = asyncComponent(() => import('./view/pages/ClientPage'))
+const AsyncNotFoundPage = asyncComponent(() => import('./view/pages/NotFound'))
+
+/**
+ * App component
  * @param {Object} store - redux global store
  * @constructor
  * @public
@@ -32,13 +40,13 @@ const App = ({ store }) => (
   <Provider store={store}>
     <Router>
       <Switch>
-        <DefaultLayout exact={true} path="/" component={LandingPage} />
-        <DefaultLayout path="/login" component={LoginFormPage} />
-        <AuthenticatedLayout path="/dashboard" component={DashboardPage} />
-        <AuthenticatedLayout path="/profile" component={ProfilePage} />
-        <AuthenticatedLayout path="/users" component={UsersPage} />
-        <AuthenticatedLayout path="/clients" component={ClientPage} />
-        <AuthenticatedLayout path="*" component={NotFoundPage} />
+        <DefaultLayout exact={true} path="/" component={AsyncLandingPage} />
+        <DefaultLayout path="/login" component={AsyncLoginPage} />
+        <AuthenticatedLayout path="/dashboard" component={AsyncDashboardPage} />
+        <AuthenticatedLayout path="/profile" component={AsyncProfile} />
+        <AuthenticatedLayout path="/users" component={AsyncUsersPage} />
+        <AuthenticatedLayout path="/clients" component={AsyncClientPage} />
+        <AuthenticatedLayout path="*" component={AsyncNotFoundPage} />
       </Switch>
     </Router>
   </Provider>
