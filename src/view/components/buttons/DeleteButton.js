0db02d269e0e66f8167c41c78b7cb25f1a08baf4
Henrik Grönvall <hgc-ab@outlook.com>
2018-12-08 21:53:03 +0100

    Refactored

--- src/view/components/buttons/DeleteButton.js
+++ src/view/components/buttons/DeleteButton.js
@@ -1,10 +1,9 @@
 /**
  * @prettier
- * @description: DeleteButton
- * @author:   Henrik Grönvall
- * @version:  0.0.1
- * @copyright:  Copyright (c) 2017 HGC AB
- * @license: The MIT License (MIT)
+ * @description: DeleteButton with confirmation alert
+ * @copyright (c) 2018 - present, HGC AB.
+ * @licence This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  */
 import React from 'react'
 import PropTypes from 'prop-types'
@@ -16,6 +15,11 @@ import { withStyles } from '@material-ui/core/styles'
 // custom component
 import AlertDialog from '../dialogs/AlertDialog'
 
+/**
+ * Component styles
+ * @param theme
+ * @returns {{root: {margin: (number|string)}}}
+ */
 const styles = theme => ({
   root: {
     margin: theme.spacing.unit
@@ -23,15 +27,12 @@ const styles = theme => ({
 })
 
 /**
- * DeleteButton using AlertDialog to confirm deletions
- * @class
- * @constructor
- * @public
+ * DeleteButton with confirmation alert
  */
 class DeleteButton extends React.PureComponent {
   /**
-   * Property type check
-   * @type {Object}
+   * Component props
+   * @type {{classes: *, variant: *, color: *, message: *, onClick: *}}
    */
   static propTypes = {
     /**
@@ -41,7 +42,7 @@ class DeleteButton extends React.PureComponent {
     /**
      * Variant of the button
      */
-    variant: PropTypes.string.isRequired,
+    variant: PropTypes.string,
     /**
      * Color of the button
      */
@@ -51,56 +52,58 @@ class DeleteButton extends React.PureComponent {
      */
     message: PropTypes.string.isRequired,
     /**
-     * Callback function
+     * Callback function to be called if deletion is confirmed
      */
-    onRemove: PropTypes.func.isRequired,
+    onClick: PropTypes.func.isRequired
   }
 
   /**
-   * Default prop types
-   * @type {Object}
+   * Default props
+   * @type {{variant: string, color: string, message: string}}
    */
   static defaultProps = {
-    variant: 'outlined',
-    color: 'secondary',
+    variant: 'text',
+    color: 'primary',
     message: 'Delete ?'
   }
 
   /**
-   * Set initial state
-   * @type {Object}
+   * Initial state
+   * @type {{open: boolean}}
    */
   state = {
     open: false
   }
 
   /**
-   * Event handler - onClick event
+   * Event handler - Button onClick event
    */
-  handleOpen = () => {
+  handleOpenAlert = () => {
     this.setState({ open: true })
   }
 
   /**
-   * Event handler - onClose event
+   * Event handler for the onClose event of the alert dialog
+   * Trigger the alert dialog to close
    */
-  handleClose = () => {
+  handleCloseAlert = () => {
     this.setState({ open: false })
   }
 
   /**
-   * Handles the response from the dialog component
-   * @param {boolean} confirm - flag indicate if delete is confirmed or not
+   * Event handler - AlertDialog onClose event
+   * @param {boolean} confirm - boolean response from AlertDialog
    */
   handleConfirmation = confirm => {
     if (confirm) {
-      this.props.onRemove()
+      this.props.onClick()
     }
+
     this.setState({ open: false })
   }
 
   /**
-   * Render the component
+   * Render component
    * @returns {*}
    */
   render() {
@@ -110,10 +113,15 @@ class DeleteButton extends React.PureComponent {
         <AlertDialog
           open={this.state.open}
           message={message}
-          onClose={this.handleClose}
-          onResponse={this.handleConfirmation}
+          onClose={this.handleCloseAlert}
+          onConfirm={this.handleConfirmation}
         />
-        <Button className={classes.root} variant={variant} color={color} onClick={this.handleOpen}>
+        <Button
+          className={classes.root}
+          variant={variant}
+          color={color}
+          onClick={this.handleOpenAlert}
+        >
           Delete
         </Button>
       </div>
