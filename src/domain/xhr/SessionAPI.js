c024ff4a0d73965128f53303d88e228714ef219d
Henrik Grönvall <hgc-ab@outlook.com>
2019-05-17 23:55:38 +0200

    More jsDoc documentation

--- src/domain/xhr/SessionAPI.js
+++ src/domain/xhr/SessionAPI.js
@@ -1,33 +1,111 @@
 /**
  * @prettier
- * @description: SessionAPI services
+ * @description: SessionAPI service
  * @copyright (c) 2018 - present, HGC AB.
- * @licence This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ * @licence This source code is licensed under the MIT license
  */
-import TokenAPI from './TokenAPI'
-import SessionEntityMgr from '../entity/SessionEntityMgr'
+import XHRService from './base/XHRService'
+import JSONValidator from '../entity/validator/JSONValidator'
+import tokenSchema from '../entity/schemas/token'
+import credentialsSchema from '../entity/schemas/credentials'
 
 /**
- * SessionAPI class
- * @class
+ * Session entity object
+ * @typedef {Object} SessionEntity - session entity object
+ * @property {Boolean} isAuth - User authenticated
+ * @property {Number} expires_in - Milliseconds for the session to be valid
+ * @property {Date} expiresAt - Date when session is expired
  */
-class SessionAPI extends TokenAPI {
-  constructor() {
-    super()
 
+/**
+ * Token entity
+ * @typedef {Object} TokenEntity
+ * @property {string} access_token - oauth access token
+ * @property {string} token_type- token type
+ * @property {number} expires_in - time in ms the token is valid
+ * @property {string} refresh_token - refresh token to issue a new token
+ * @property {string} scope - scope of token
+ */
+
+/**
+ * SessionAPI class providing an interface to create, refresh and remove a session
+ *
+ * The class extends the XHRService class which is a HTTP client wrapper providing
+ * access to a store interface for storage of tokens in the HTTP client
+ *
+ * @example
+ * const sessionAPI = new SessionAPI()
+ * const session = await sessionAPI.createSession(username, password)
+ * const newSession = await sessionAPI.refreshSession()
+ * sessionAPI.removeSession()
+ *
+ * Note that the session, aka token is persisted in
+ * some global state and refreshSession() method is
+ * accessing that state
+ *
+ * @extends XHRService
+ */
+class SessionAPI extends XHRService {
+  /**
+   * Session API constructor
+   * @param {Object} options - configuration options
+   * @param {String} options.url - resource url for the API
+   */
+  constructor({ url = '/oauth/tokens' } = {}) {
+    super()
     /**
-     * Token entity manager
-     * @type {SessionEntityMgr}
+     * Resource url for tokens
+     * @type {String}
      * @private
      */
-    this._sessionMgr = new SessionEntityMgr()
+    this._tokenURL = url
+    /**
+     * Instance validation function
+     * @type {JSONValidator}
+     * @private
+     */
+    this._tokenValidator = new JSONValidator(tokenSchema)
+    /**
+     * Instance default entity
+     * @type {TokenEntity}
+     * @private
+     */
+    this._token = this._tokenValidator.defaultEntity
+    /**
+     * Instance credentials validation function
+     * @type {JSONValidator}
+     * @private
+     */
+    this._credentialsValidator = new JSONValidator(credentialsSchema)
+  }
+
+  /**
+   * Getter to retrieve the instance entity
+   * @returns {TokenEntity} - instance entity object
+   * @private
+   */
+  get token() {
+    return this._token
+  }
+
+  /**
+   * Setter to set the instance entity
+   * @param {TokenEntity} entity - entity object to update instance entity
+   * @private
+   */
+  set token(entity) {
+    try {
+      this._token = this._tokenValidator.isValid(entity)
+    } catch (err) {
+      throw err
+    }
   }
 
   /**
    * Transform a token to a session, validate the new session and store it as instance entity
    * @param {TokenEntity} token - a token entity to be transformed
-   * @returns {SessionEntity|ValidationException} - the instance entity or a validation exception
+   * @returns {SessionEntity} - a session entity
+   * @private
    */
   toSessionObject(token) {
     const { expires_in } = token
@@ -41,30 +119,53 @@ class SessionAPI extends TokenAPI {
   }
 
   /**
-   * Create a new session
+   * Transform instance credentials to credentials with password grant data
+   * @param {Object} credentials - credentials object
+   * @param {String} credentials.username - username
+   * @param {String} credentials.password - password
+   * @returns {Object}
+   * @private
+   */
+  toPasswordGrant({ username, password }) {
+    return {
+      username,
+      password,
+      client_id: process.env.REACT_APP_CLIENT_ID,
+      grant_type: 'password'
+    }
+  }
+
+  /**
+   * Create a token by posting credentials with password grant
    * @param {String} username - username
    * @param {String} password - password
-   * @returns {Promise<SessionEntity|ValidationException>} - a promise resolver to session or a validation exception
+   * @returns {Promise<SessionEntity>} - a promise resolved to a session entity
+   * @throws {ValidationException} throws an error on failure
+   * @public
    */
   async createSession(username, password) {
     try {
-      const token = await super.createToken(username, password)
-      this._sessionMgr.entity = this.toSessionObject(token)
-      return this._sessionMgr.entity
+      const credentials = this._credentialsValidator.isValid({ username, password })
+      this.token = await super.post(this._tokenURL, this.toPasswordGrant(credentials))
+      super.setStoreItem(this.token)
+      return this.toSessionObject(this.token)
     } catch (err) {
       throw err
     }
   }
 
   /**
-   * Refresh the existing session
-   * @returns {Promise<SessionEntity|ValidationException>} - a promise resolved to session or a validation exception
+   * Refresh session by posting a refresh token
+   * @returns {Promise<SessionEntity>} - a promise resolved to a session entity
+   * @throws {ValidationException} throws an error on failure
+   * @public
    */
   async refreshSession() {
     try {
-      let token = await super.refreshToken()
-      this._sessionMgr.toSessionObject(token)
-      return this._sessionMgr.entity
+      const { refresh_token } = super.getStoreItem()
+      this.token = await super.post(this._tokenURL, refresh_token)
+      super.setStoreItem(this.token)
+      return this.toSessionObject(this.token)
     } catch (err) {
       throw err
     }
@@ -72,12 +173,13 @@ class SessionAPI extends TokenAPI {
 
   /**
    * Remove session
-   * @returns {Promise<{SessionEntity}>}
+   * @returns {Object} an empty object
+   * @public
    */
-  async removeSession() {
-    this._sessionMgr.entity = super.removeToken()
-    return this._sessionMgr.entity
+  removeSession() {
+    super.removeStoreItem()
+    return (this._token = {})
   }
 }
 
-export default new SessionAPI()
+export default SessionAPI
